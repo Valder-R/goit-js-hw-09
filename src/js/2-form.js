08746721154c0bb1ee1a8ebb70559a081fa36391
Volodymyr_Rostovtsev <fortik1510@gmail.com>
2025-03-21 19:41:52 +0100

    implemented tasks

---
+++ src/js/2-form.js
@@ -0,0 +1,26 @@
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+const formData = JSON.parse(localStorage.getItem(localStorageKey)) ?? {
+    email: "",
+    message: "",
+};
+form.elements.email.value = formData.email;
+form.elements.message.value = formData.message;
+
+form.addEventListener("input", (event) => {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(localStorageKey,JSON.stringify(formData))
+});
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    if (formData.email != "" && formData.message != "") {
+        localStorage.removeItem(localStorageKey);
+        console.log(formData);
+        formData.email = "";
+        formData.message = "";
+        form.reset();
+    } else {
+        alert("Fill please all fields");
+    }
+});
